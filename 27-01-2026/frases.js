a7084032b204b2deaeb87939380762264861bdc4
Joao Santana <joao.p.santana25@aluno.senai.br>
2026-01-27 16:52:38 -0300

    Testes com F string do javascript

---
+++ 27-01-2026/frases.js
@@ -0,0 +1,13 @@
+let frase = "Ele disse: \"Oi, tudo bem?\"";
+
+let nome = 'João'
+let saudacao =`Olá, ${nome}!`;
+
+console.log(saudacao)
+
+let x =10;
+let y = 5;
+
+let resultado = `A soma de ${x} e ${y} é ${y+x}.`;
+
+console.log(resultado)
