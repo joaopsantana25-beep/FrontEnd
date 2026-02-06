40f94c742593b5079edfd101accfe9566af305e5
João Santana <jpsantana12118@gmail.com>
2026-02-06 16:00:28 -0300

    Exercícios booleanos

--- 04-02-2026/Exercicios e Testes - JS/Testes_Booleanos.js
+++ 04-02-2026/Exercicios e Testes - JS/Testes_Booleanos.js
@@ -1,1 +1,18 @@
 //Espaço para testar valores de verdadeiro e falso
+
+let base =2;
+let expoente =3;
+let resultado = base ** expoente;
+
+console.log(base++);
+console.log(base);
+
+console.log(++base);
+console.log(base);
+
+
+console.log(base--);
+console.log(base);
+
+console.log(--base);
+console.log(base);
